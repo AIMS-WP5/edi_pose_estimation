feaf225dc87a56bae6cd5794b4e7f0c4af863d62
Arnis Prieditis <arnis.prieditis@edi.lv>
2025-02-06 13:09:24 +0200

    add test result logging

--- rs_pose_est_aruco.py
+++ rs_pose_est_aruco.py
@@ -3,10 +3,25 @@ import numpy as np
 import cv2
 import time
 from pose_estimator import estimate_pose, estimate_pose_fast, estimation_setup
+import csv
+import datetime
 
-# ground truth bottle position relative to aruco board, meters and radians
-BOARD_BOTTLE_TVEC = np.array([0.07, 0.00, -0.10])
-BOARD_BOTTLE_RVEC = np.array([0.00, 0.00, np.pi/4])
+# "ground truth" bottle position relative to aruco board, meters and radians
+BOARD_BOTTLE_TVEC = np.array([0.07, 0.00, 0.00])
+BOARD_BOTTLE_RVEC = np.array([0.00, np.pi, 0.0])
+
+# setup csv file for data recording/logging
+t = datetime.datetime.now()
+timestamp = f"{t.year}_{t.month}_{t.day}_{t.hour}_{t.minute}_{t.second}"
+file = open(f"./testing/{timestamp}.csv", "w", newline="")
+field_names = [
+    "True T x", "True T y", "True T z", "True R x", "True R y", "True R z", 
+    "T x", "T y", "T z", "R x", "R y", "R z", 
+    "Fitness", "Inlier rmse", "Correspondence set size", 
+    "Detection time", "Total time"
+]
+writer = csv.DictWriter(file, fieldnames=field_names)
+writer.writeheader()
 
 # Load ArUco dictionary
 aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
@@ -57,6 +72,7 @@ config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 pipeline.start(config)
 
 try:
+    prev_time = time.time()
     while True:
 
         # Wait for both frames: depth and color
@@ -73,14 +89,15 @@ try:
         # cv2.imwrite("./testing/color.png", color_image)
         # cv2.imwrite("./testing/depth.png", depth_image)
 
-        # result_poses = estimate_pose(color_img=color_image, depth_img=depth_image, Kdepth=camera_matrix)
+        detection_start = time.time()
         result_poses = estimate_pose_fast(color_image, depth_image, camera_matrix, det_model, cls_idxs, sam, DOWN_SAMPLE_SIZE, ref_pcd)
+        detection_time = time.time() - detection_start
         print("Detected poses:",result_poses)
-        # with open("./testing/camera_intrinsics.txt", "w") as file:
-        #     file.write(str(camera_matrix))
-        #     file.write(f"\n{result_poses}")
+        if result_poses == []:
+            continue
+        axes_img = color_image.copy()
         for pose in result_poses:
-            cv2.drawFrameAxes(color_image, camera_matrix, dist_coeffs, pose[1], pose[0], 0.05, 1)
+            cv2.drawFrameAxes(axes_img, camera_matrix, dist_coeffs, pose[1], pose[0], 0.05, 1)
 
         # detect board position
         gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
@@ -92,16 +109,39 @@ try:
             retval, rvec, tvec = cv2.aruco.estimatePoseBoard(
                 corners, ids, board, camera_matrix, dist_coeffs, rvec, tvec, False
             )
-            # print(f"Rotation:\n{rvec}")
-            # print(f"Translation:\n{tvec}")
             R_board, _ = cv2.Rodrigues(rvec)
             R_bottle, _ = cv2.Rodrigues(BOARD_BOTTLE_RVEC)
             R = R_board.dot(R_bottle)
             bottle_rvec, _ = cv2.Rodrigues(R)
             bottle_tvec = tvec + R_board.dot(BOARD_BOTTLE_TVEC.reshape(3,1))
-            cv2.drawFrameAxes(color_image, camera_matrix, dist_coeffs, bottle_rvec, bottle_tvec, 0.05, 2)
+            cv2.drawFrameAxes(axes_img, camera_matrix, dist_coeffs, bottle_rvec, bottle_tvec, 0.05, 2)
+        
+        # write results to log file
+        curr_time = time.time()
+        total_time = curr_time - prev_time
+        prev_time = curr_time
+        row = {
+            "True T x": bottle_tvec[0][0],
+            "True T y": bottle_tvec[1][0],
+            "True T z": bottle_tvec[2][0],
+            "True R x": bottle_rvec[0][0],
+            "True R y": bottle_rvec[1][0],
+            "True R z": bottle_rvec[2][0],
+            "T x": result_poses[0][0][0],
+            "T y": result_poses[0][0][1],
+            "T z": result_poses[0][0][2],
+            "R x": result_poses[0][1][0],
+            "R y": result_poses[0][1][1],
+            "R z": result_poses[0][1][2],
+            "Fitness": result_poses[0][2].fitness,
+            "Inlier rmse": result_poses[0][2].inlier_rmse,
+            "Correspondence set size": len(result_poses[0][2].correspondence_set[0]),
+            "Detection time": detection_time,
+            "Total time": total_time
+        }
+        writer.writerow(row)
 
-        cv2.imshow("Pose estimation", color_image)
+        cv2.imshow("Pose estimation", axes_img)
         # Exit on 'q' key
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
@@ -112,3 +152,4 @@ finally:
     # Stop streaming
     pipeline.stop()
     cv2.destroyAllWindows()
+    file.close()
