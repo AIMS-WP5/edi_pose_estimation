0c727f99bc40b292bc8a9028d96ae4be3aa9dd46
Arnis Prieditis <arnis.prieditis@edi.lv>
2025-02-20 11:53:09 +0200

    rewrite estimator as class

---
+++ src/estimator.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+import open3d as o3d
+from ultralytics import SAM
+from ultralytics import RTDETR
+import yaml
+import os
+
+
+class Estimator:
+    def __init__(self):
+        self.det_model = RTDETR('rtdetr-x.pt')
+        self.cls_idxs = [id for id,name in det_model.names.items() if name in ['bottle','cup']]
+        self.sam = SAM('mobile_sam.pt')
+        curr_dir = os.path.dirname(os.path.abspath(__file__))
+        config_path = curr_dir + "/../config/estimator.yml"
+        with open(config_path, "r") as config_file:
+            config = yaml.safe_load(config_file)
+        ref_pdc_path = config["ref_pcd_path"] # must be absolute path
+        self.down_smaple_size = config["down_sample_size"]
+        ref_pcd = o3d.io.read_point_cloud(ref_pdc_path)
+        cnt = np.asarray(ref_pcd.points).shape[0]
+        ref_pcd.colors = o3d.utility.Vector3dVector(np.repeat([[1,0,0]],cnt,axis = 0).astype(np.float32))
+        self.ref_pcd = ref_pcd.voxel_down_sample(self.down_smaple_size)
+        self.fitness_limit = config["fitness_limit"]
+
+    def estimate(self, color_img, depth_img, camera_matrix):
+        pts = cv2.rgbd.depthTo3d(depth_img, camera_matrix)
+        results = self.det_model(color_img, classes = self.cls_idxs)
+        det_result = results[0]
+        if len(det_result.boxes) == 0:
+            return []
+        sam_result = self.sam.predict(color_img, bboxes = det_result.boxes.xyxy)[0]
+        results = [self.estimate_pose_for_mask(pts, color_img, mask.cpu().numpy(), self.down_smaple_size, self.ref_pcd) for mask in sam_result.masks.data]
+        ## Filter the results by fitness score. Fitness ranges from 0 to 1,
+        #  and shows the inlier proportion. For an object, even 0.5 can be
+        #  a successful match, since the object can be seen from one side.
+        filtered_results = list(filter(lambda x: x[2].fitness > self.fitness_limit, results))
+
+        return results
+
+    def estimate_pose_for_mask(self, pts, color_img, mask):
+        """
+        Estimate the pose for an object in the point cloud.
+        - pts: the point cloud with shape (height,width,coord) with type np.float32
+        - color_img: the colors for the pts points with shape (height,width,3)
+        - mask: mask for the object with shape (height,width) with type np.bool
+
+        Returns a tuple:
+            tvec - translation
+            rvec - Rodrigues vector for rotation
+            reg_p2p - the result object from Open3D (open3d.pipelines.registration.RegistrationResult)
+        """
+        objpts = pts[mask,:]
+        color = color_img[mask,:]
+
+        #remove NaNs (e.g. wrong depth pixels)
+        verts = objpts.reshape((-1,3))
+        idx = ~np.isnan(verts).any(axis=1)
+        verts = verts[idx,:]
+        color = color[idx,:]
+
+        # create PCD
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(verts)
+        pcd.colors = o3d.utility.Vector3dVector(color/255)
+
+        # downsample the PCD
+        pcd_ds = pcd.voxel_down_sample(self.down_smaple_size)
+
+        # default transformation is around the mean of the object, with identity rotation
+        pts_mean = np.mean(np.asarray(pcd_ds.points),axis=0)
+        initial_transform = np.block([[np.identity(3), np.asmatrix(pts_mean).T],[0,0,0,1]])
+
+        # run pose estimation with different outlier margins
+        reg_p2p = o3d.pipelines.registration.registration_icp(
+            self.ref_pcd, pcd_ds, 1, initial_transform,
+            o3d.pipelines.registration.TransformationEstimationPointToPoint(),
+            o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=100))
+
+        reg_p2p = o3d.pipelines.registration.registration_icp(
+            self.ref_pcd, pcd_ds, 0.01, reg_p2p.transformation,
+            o3d.pipelines.registration.TransformationEstimationPointToPoint(),
+            o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=100))
+
+        # create rvec and tvec
+        tvec = reg_p2p.transformation[0:3,3]
+        rvec,_ = cv2.Rodrigues(reg_p2p.transformation[0:3,0:3])
+        rvec = rvec.T[0,:]
+        return tvec,rvec,reg_p2p
---
+++ src/rs_est_testing.py
@@ -0,0 +1,147 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import time
+import estimator
+import csv
+import datetime
+import os
+
+def main():
+    ## setup csv file for data recording/logging
+    t = datetime.datetime.now()
+    timestamp = f"{t.year}_{t.month}_{t.day}_{t.hour}_{t.minute}_{t.second}"
+    curr_dir = os.path.dirname(os.path.abspath(__file__))
+    log_file_path = curr_dir + f"/../testing/{timestamp}.csv"
+    log_file = open(log_file_path, "w", newline="")
+    field_names = [
+        "True T x", "True T y", "True T z", "True R x", "True R y", "True R z", 
+        "T x", "T y", "T z", "R x", "R y", "R z", 
+        "Fitness", "Inlier rmse", "Correspondence set size", 
+        "Detection time", "Total time"
+    ]
+    writer = csv.DictWriter(log_file, fieldnames=field_names)
+    writer.writeheader()
+
+    ## ArUco setup
+    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+    parameters = cv2.aruco.DetectorParameters()
+    marker_length = 0.026 # m
+    marker_separation = 0.013 # m
+    board = cv2.aruco.GridBoard((5,7), marker_length, marker_separation, aruco_dict)
+    # "ground truth" bottle position relative to aruco board, meters and radians
+    BOARD_BOTTLE_TVEC = np.array([-0.037, 1.50, -0.039])
+    BOARD_BOTTLE_RVEC = np.array([0.00, -np.pi/2, 0.0])
+    # BOARD_BOTTLE_RVEC = np.array([np.pi/np.sqrt(2), 0.00, np.pi/np.sqrt(2)])
+
+    ## estimator setup
+    sam_seg_estimator = estimator.Estimator()
+
+    ## camera setup
+    pipeline = rs.pipeline()
+    config = rs.config()
+    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    pipeline_profile = config.resolve(pipeline_wrapper)
+    device = pipeline_profile.get_device()
+    # device_product_line = str(device.get_info(rs.camera_info.product_line))
+    # Get intrinsic matrix of camera
+    intr = pipeline_profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
+    fx = float(intr.fx) # Focal length of x
+    fy = float(intr.fy) # Focal length of y
+    ppx = float(intr.ppx) # Principle Point Offsey of x (aka. cx)
+    ppy = float(intr.ppy) # Principle Point Offsey of y (aka. cy)
+    axs = 0.0 # Axis skew
+    camera_matrix = np.array([
+        [fx, axs, ppx],
+        [0.0, fy, ppy],
+        [0.0, 0.0, 1.0]])
+    dist_coeffs = np.asanyarray(intr.coeffs)
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    # config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 6)
+    # config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 6)
+    # config.enable_device_from_file("/home/arnis/AIMS/aitools/pose_estimation/rs_bags/20250210_154204.bag")
+    # create align object
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+    # Start streaming
+    pipeline.start(config)
+
+    try:
+        prev_time = time.time()
+        while True:
+            # Wait for both frames: depth and color
+            frames = pipeline.wait_for_frames()
+            aligned_frames = align.process(frames)
+            depth_frame = aligned_frames.get_depth_frame()
+            color_frame = aligned_frames.get_color_frame()
+            if not depth_frame or not color_frame:
+                continue
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
+
+            detection_start = time.time()
+            result_poses = sam_seg_estimator.estimate(color_image, depth_image, camera_matrix)
+            detection_time = time.time() - detection_start
+            print("Detected poses:",result_poses)
+            if result_poses == []:
+                continue
+            axes_img = color_image.copy()
+            for pose in result_poses:
+                cv2.drawFrameAxes(axes_img, camera_matrix, dist_coeffs, pose[1], pose[0], 0.05, 1)
+
+            # detect board position
+            gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+            corners, ids, rejected = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+            corners, ids, rejected, recovered_ids = cv2.aruco.refineDetectedMarkers(gray, board, corners, ids, rejected, camera_matrix, dist_coeffs)
+            rvec = None
+            tvec = None
+            if ids is not None:
+                retval, rvec, tvec = cv2.aruco.estimatePoseBoard(
+                    corners, ids, board, camera_matrix, dist_coeffs, rvec, tvec, False
+                )
+                R_board, _ = cv2.Rodrigues(rvec)
+                R_bottle, _ = cv2.Rodrigues(BOARD_BOTTLE_RVEC)
+                R = R_board.dot(R_bottle)
+                bottle_rvec, _ = cv2.Rodrigues(R)
+                bottle_tvec = tvec + R_board.dot(BOARD_BOTTLE_TVEC.reshape(3,1))
+                cv2.drawFrameAxes(axes_img, camera_matrix, dist_coeffs, bottle_rvec, bottle_tvec, 0.05, 2)
+            
+            # write results to log file
+            curr_time = time.time()
+            total_time = curr_time - prev_time
+            prev_time = curr_time
+            row = {
+                "True T x": bottle_tvec[0][0],
+                "True T y": bottle_tvec[1][0],
+                "True T z": bottle_tvec[2][0],
+                "True R x": bottle_rvec[0][0],
+                "True R y": bottle_rvec[1][0],
+                "True R z": bottle_rvec[2][0],
+                "T x": result_poses[0][0][0],
+                "T y": result_poses[0][0][1],
+                "T z": result_poses[0][0][2],
+                "R x": result_poses[0][1][0],
+                "R y": result_poses[0][1][1],
+                "R z": result_poses[0][1][2],
+                "Fitness": result_poses[0][2].fitness,
+                "Inlier rmse": result_poses[0][2].inlier_rmse,
+                "Correspondence set size": len(result_poses[0][2].correspondence_set),
+                "Detection time": detection_time,
+                "Total time": total_time
+            }
+            writer.writerow(row)
+
+            cv2.imshow("Pose estimation", axes_img)
+            # Exit on 'q' key
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+            # time.sleep(0.1)
+    finally:
+        pipeline.stop()
+        cv2.destroyAllWindows()
+        log_file.close()
+
+
+if __name__ == '__main__':
+    main()
