edcbd5af4417ceddf0e9811c2754b9e2d245bc46
Arnis Prieditis <arnis.prieditis@edi.lv>
2026-03-06 15:49:47 +0200

    fix position conversion to m and publish PoseStamped instead of Pose

--- src/run_rotation_estimator.py
+++ src/run_rotation_estimator.py
@@ -7,21 +7,18 @@ from estimator2 import Estimator
 
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseStamped
 import scipy
 
 import pyrealsense2 as rs
-
-import sys
-import io
-
-# sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-# sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
 
 def camera_setup():
     pipeline = rs.pipeline()
     config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.rgb8, 30)
+    # config.enable_device_from_file("/home/arnis/aims/rotation_predict/20260217_161517.bag")
     pipeline_wrapper = rs.pipeline_wrapper(pipeline)
     pipeline_profile = config.resolve(pipeline_wrapper)
     # Get intrinsic matrix of camera
@@ -37,9 +34,6 @@ def camera_setup():
         [0.0, 0.0, 1.0]])
     print("Camera matrix:", camera_matrix)
     dist_coeffs = np.asanyarray(intr.coeffs)
-    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.rgb8, 30)
-    # config.enable_device_from_file("/home/arnis/aims/rotation_predict/20260217_161517.bag")
     # create align object
     align_to = rs.stream.infrared # for d405, for other cameras use rs.stream.color
     align = rs.align(align_to)
@@ -54,7 +48,7 @@ def camera_setup():
 def main():
 
     node = rclpy.create_node("realsense_pose_estimation")
-    pose_pub = Node.create_publisher(node, Pose, "/bottle_pose", 10)
+    pose_pub = Node.create_publisher(node, PoseStamped, "/bottle_pose", 10)
 
     pipeline, K, align, depth_scale, dist_coeffs = camera_setup()
     try:
@@ -95,20 +89,20 @@ def main():
             print(f"Estimator result: {'None' if res_est is None else 'OK'}")
             if res_est:
                 print("pose (estimator):\n", res_est['pose'])
-                position_rez = 0.001 * np.array(res_est["tvec"]) # no mm uz m
+                position_rez = depth_scale * np.array(res_est["tvec"]) # lai parveidotu uz m
                 est_pose_rvec = np.array(res_est["rvec"])
                 est_pose_rmat, _ = cv2.Rodrigues(est_pose_rvec)
                 est_pose_quat = scipy.spatial.transform.Rotation.from_matrix(est_pose_rmat).as_quat() # [x y z w]
 
                 
-                pose_msg = Pose()
-                pose_msg.position.x = position_rez[0]
-                pose_msg.position.y = position_rez[1]
-                pose_msg.position.z = position_rez[2]
-                pose_msg.orientation.x = est_pose_quat[0]
-                pose_msg.orientation.y = est_pose_quat[1]
-                pose_msg.orientation.z = est_pose_quat[2]
-                pose_msg.orientation.w = est_pose_quat[3]
+                pose_msg = PoseStamped()
+                pose_msg.pose.position.x = position_rez[0]
+                pose_msg.pose.position.y = position_rez[1]
+                pose_msg.pose.position.z = position_rez[2]
+                pose_msg.pose.orientation.x = est_pose_quat[0]
+                pose_msg.pose.orientation.y = est_pose_quat[1]
+                pose_msg.pose.orientation.z = est_pose_quat[2]
+                pose_msg.pose.orientation.w = est_pose_quat[3]
                 pose_pub.publish(pose_msg)
 
                 axes_img = color.copy()
